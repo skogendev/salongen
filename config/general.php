84c46f9c953c74d77d57fea97b548761bc66e23e
magnusskogen <magnus@skogen.io>
2018-12-12 11:51:30 +0100

    Initial commit

---
+++ config/general.php
@@ -0,0 +1,65 @@
+<?php
+/**
+ * General Configuration
+ *
+ * All of your system's general configuration settings go in here. You can see a
+ * list of the available settings in vendor/craftcms/cms/src/config/GeneralConfig.php.
+ *
+ * @see craft\config\GeneralConfig
+ */
+
+return [
+    // Global settings
+    '*' => [
+        // Default Week Start Day (0 = Sunday, 1 = Monday...)
+        'defaultWeekStartDay' => 0,
+
+        // Enable CSRF Protection (recommended)
+        'enableCsrfProtection' => true,
+
+        // Whether generated URLs should omit "index.php"
+        'omitScriptNameInUrls' => true,
+
+        // Control Panel trigger word
+        'cpTrigger' => 'admin',
+
+        // The secure key Craft will use for hashing and encrypting data
+        'securityKey' => getenv('SECURITY_KEY'),
+
+        'useProjectConfigFile' => true,
+
+        // Static asset versioning. This number is set by gulp.
+        'environmentVariables' => array(
+            'staticAssetsVersion' => 12,
+        ),
+
+    ],
+
+    // Dev environment settings
+    'dev' => [
+        // Base site URL
+        'siteUrl' => null,
+        'baseUrl' => 'http://localhost:8080',
+
+        // Dev Mode (see https://craftcms.com/support/dev-mode)
+        'devMode' => true,
+        'environmentVariables' => array(
+            'staticAssetsVersion' => time(),
+        ),
+    ],
+
+    // Staging environment settings
+    'staging' => [
+        // Base site URL
+        'siteUrl' => null,
+        'environmentVariables' => array(
+            'staticAssetsVersion' => time(),
+        ),
+    ],
+
+    // Production environment settings
+    'production' => [
+        // Base site URL
+        'siteUrl' => null,
+    ],
+];
--- gulpfile.js
+++ gulpfile.js
@@ -1,19 +1,80 @@
-var gulp = require('gulp');
-var purgecss = require('gulp-purgecss')
-var postcss = require('gulp-postcss');
-var tailwindcss = require('tailwindcss');
+/*
 
-var destination = 'web/assets/'
+A quick note regarding environments:
 
-gulp.task('css', function () {
-  return gulp.src('src/css/styles.css')
+1) Set (mode.prod()) or (mode.dev()) before tasks if that task is only needed for that environment
+2) Run gulp --prod before deployment to prod
+
+*/
+
+var browserSync = require('browser-sync').create(),
+    errorHandler = require('gulp-error-handle'),
+    gulp = require('gulp'),
+    mode = require('gulp-mode')({
+      modes: ['prod','dev'],
+      default: 'dev'
+    }),
+    php = require('gulp-connect-php'),
+    pkg = require('./package.json'),
+    replace = require('gulp-replace'),
+    purgecss = require('gulp-purgecss'),
+    postcss = require('gulp-postcss'),
+    tailwindcss = require('tailwindcss'),
+    watch = require('gulp-watch');
+
+/* File paths */
+var siteUrl = 'http://starter.localhost/',
+    pathCraftConfig = 'config/'
+    src = 'src/',
+    dest = 'web/assets/';
+
+/* Building dist files from source files */
+gulp.task('default', function () {
+  gulp.src(pkg.paths.src.css +  'styles.css')
+    .pipe(errorHandler())
     .pipe(postcss([
       tailwindcss('tailwind.js'),
       require('autoprefixer'),
     ]))
-    .pipe(purgecss({
-        content: ['templates/**/*.*']
+    .pipe((mode.prod(purgecss({
+        content: [pkg.paths.templates + '**/*.*']
       })
-    )
-    .pipe(gulp.dest(destination));
+    )))
+    .pipe(gulp.dest(pkg.paths.dest.css));
+  if (mode.prod()) {
+    gulp.start('asset-bust')
+  }
+});
+
+/* Creating PHP server for browserSync */
+gulp.task('php', function() {
+  php.server({ base: 'build', port: 8010, keepalive: true});
+});
+
+/* Setting up browserSync */
+gulp.task('browser-sync', ['php'], function(){
+  browserSync.init({
+    proxy: siteUrl,
+    port: 8080,
+    open: true,
+    notify: false
+  });
+});
+
+/* Static assets versioning */
+gulp.task('asset-bust', function(){
+  gulp.src(pkg.paths.craft.config + '/general.php')
+  .pipe(replace(/'staticAssetsVersion' => (\d+),/g, function(match, p1, offset, string) {
+    p1++;
+    return "'staticAssetsVersion' => " + p1 + ",";
+  }))
+  .pipe(gulp.dest(pkg.paths.craft.config));
+});
+
+/* The task. Run gulp watch from CLI */
+gulp.task('watch', ['browser-sync'], function(){
+  watch(pkg.paths.src.base+'/**/*',function(){
+      gulp.start('default');
+      browserSync.reload();
+  });
 });
