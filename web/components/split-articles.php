df60d15933c27663c84ea0809d5443ad92d5a039
magnusskogen <magnus@skogen.io>
2019-05-31 12:35:24 +0200

    Adjustments for mobile

--- web/components/split-articles.php
+++ web/components/split-articles.php
@@ -2,7 +2,7 @@
   <div class="section bg-split-left">
     <div class="container bg-segl">
       <div class="u-flexgrid-g justify-between text-center">
-        <a href="/artikkel.php" class="col-12 lg:col-5 xl:col-5 mb-16">
+        <div class="col-12 lg:col-5 xl:col-5 mb-16">
           <div class="overflow-hidden"><img src="/assets/img/tmp4.jpg" class="mb-8 lazyload-img lazyload"></div>
           <div class="w-4/5 mx-auto">
             <div class="heading-sm text-eple mb-4">Mat & drikke</div>
@@ -11,8 +11,8 @@
               <p>Salongen er et personlig og inspirerende konferansehus med et stort hjerte for å skreddersy arrangementer av høy kvalitet. Salongen har møterom og fasiliteter som passer fint for både små møter og større arrangementer. </p>
             <a href="/artikke.php" class="button mt-4">Mer om mat og drikke</a>
           </div>
-        </a>
-        <a href="/artikkel.php" class="col-12 lg:col-5 xl:col-5">
+        </div>
+        <div class="col-12 lg:col-5 xl:col-5">
           <div class="overflow-hidden"><img src="/assets/img/tmp4.jpg" class="mb-8 lazyload-img lazyload"></div>
           <div class="w-4/5 mx-auto lg:bg-krem-light">
             <div class="heading-sm text-eple mb-4">Mat & drikke</div>
@@ -21,7 +21,7 @@
               <p>Salongen er et personlig og inspirerende konferansehus med et stort hjerte for å skreddersy arrangementer av høy kvalitet. Salongen har møterom og fasiliteter som passer fint for både små møter og større arrangementer. </p>
               <a href="/artikkel.php" class="button mt-4">Mer om mat og drikke</a>
           </div>
-        </a>
+        </div>
       </div>
     </div>
   </div>
