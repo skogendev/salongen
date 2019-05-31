5ddd0c2e159b5d7344f53070e421605da63d0103
magnusskogen <magnus@skogen.io>
2019-05-31 15:29:51 +0200

    Adjustments

--- web/components/home-intro.php
+++ web/components/home-intro.php
@@ -1,33 +1,48 @@
 <div class="section bg-krem-light py-0" style="padding: 0;">
-  <div class="h-full overflow-hidden absolute w-full">
-    <div class="absolute container w-full pin-l u-flexgrid-g" style="min-height: 90vh">
-      <div class="col-2">
-        <div class="overflow-hidden relative" style="transform: scale(2); transform-origin: top right; position: relative; top: 10vh;">
-          <div class="sticky-image">
-            <img data-src="/assets/img/tmp19.jpg" class="inview lazyload-img lazyload">
+  <div class="h-full overflow-hidden absolute w-full mx-auto">
+    <div class="container w-full pin-l u-flexgrid-g" style="min-height: 90vh">
+      <div class="col-1">
+        <div style="transform: scale(5); transform-origin: top right; position: relative; top: 10vh;">
+          <div class="overflow-hidden relative">
+            <div class="sticky-image*">
+              <img data-src="/assets/img/tmp19.jpg" class="inview lazyload-img lazyload">
+            </div>
           </div>
         </div>
       </div>
-      <div class="col-5 col-off-5">
-        <div class="mt-12 sticky-image">
-          <img src="/assets/img/tmp21.jpg" class="inview mt-32 lazyload-img lazyload">
+      <div class="col-5 col-off-6">
+        <div class="mt-12">
+          <div class="overflow-hidden">
+            <div class="sticky-image* reverse">
+              <!--<img src="/assets/img/tmp21.jpg" class="inview mt-32 lazyload-img lazyload">-->
+              <video src="/assets/video/video1.mp4" autoplay loop muted>
+            </div>
+          </div>
           <div class="text-sm mt-4">
             <p><span class="font-serif text-eple">FIG 2</span><br>
             Få den beste maten du kan tenke deg</p>
           </div>
         </div>
       </div>
-      <div class="col-3 col-off-2 -mt-32 sticky-image">
-        <img src="/assets/img/tmp20.jpg" class="inview mt-8 lazyload-img lazyload">
+      <div class="col-3 col-off-2 -mt-16">
+        <div class="overflow-hidden">
+          <div class="sticky-image*">
+            <img src="/assets/img/tmp20.jpg" class="inview mt-8 lazyload-img lazyload hidden">
+            <video src="/assets/video/video2.mp4" autoplay loop muted class="w-full h-auto">
+
+          </div>
+        </div>
         <div class="text-sm mt-4">
           <p><span class="font-serif text-eple">FIG 1</span><br>
             Vi skreddersyr alle arrangement</p>
         </div>
       </div>
-      <div class="col-2 col-off-8 mt-16">
-        <div class="sticky-image">
-          <div style="transform: scale(1.5); transform-origin: bottom left;">
-            <img src="/assets/img/tmp1.jpg" class="inview lazyload-img lazyload">
+      <div class="col-2 col-off-8 mt-0">
+        <div class="overflow-hidden*">
+          <div class="sticky-image*">
+            <div style="transform: scale(1.5); transform-origin: bottom left;">
+              <img src="/assets/img/tmp2.jpg" class="inview lazyload-img lazyload">
+            </div>
           </div>
         </div>
       </div>
@@ -36,7 +51,7 @@
   <div class="container bg-segl relative">
     <div class="u-flexgrid-g " style="min-height: 90vh">
       <div class="col-12 lg:col-8 xl:col-off-2 xl:col-4 text-left pt-32">
-        <h1 class="heading-xl mb-8 js-load-stagger pt-12">
+        <h1 class="heading-xl mb-8 js-load-stagger pt-20">
           Det personlige konferansehuset du har ventet på
         </h1>
 
