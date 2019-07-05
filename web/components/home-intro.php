431b39d71d5dbc4d58c9492e757833c90722b55f
magnusskogen <magnus@skogen.io>
2019-07-05 15:43:39 +0200

    Adjustments

--- web/components/home-intro.php
+++ web/components/home-intro.php
@@ -1,8 +1,8 @@
 <div class="section bg-krem-light py-0" style="padding: 0;">
   <div class="h-full overflow-hidden absolute w-full mx-auto">
-    <div class="container w-full pin-l  items-end u-flexgrid-g opacity-0 lg:opacity-100" style="min-height: 90vh">
-      <div class="col-1">
-        <div style="transform: scale(10); transform-origin: bottom right;">
+    <div class="container w-full pin-l  items-end u-flexgrid-g" style="min-height: 90vh">
+      <div class="col-12 lg:col-1 front-first-img">
+        <div class="front-img-1">
           <div class="overflow-hidden relative">
             <div class="sticky-image*">
               <img data-src="/assets/img/tmp19.jpg" class="inview lazyload-img lazyload">
@@ -15,8 +15,7 @@
           <div style="transform: scale(1.3); transform-origin: bottom left;">
             <div class="overflow-hidden">
               <div class="sticky-image* reverse">
-                <!--<img src="/assets/img/tmp21.jpg" class="inview mt-32 lazyload-img lazyload">-->
-                <video src="/assets/video/video1.mp4" autoplay loop muted>
+                <video src="/assets/video/video1.mp4" autoplay loop muted class="hidden lg:block">
               </div>
             </div>
           </div>
@@ -24,10 +23,9 @@
         </div>
       </div>
       <div class="col-6 lg:col-4 col-off-1 lg:col-off-2">
-        <div class="overflow-hidden">
+        <div class="overflow-hidden ">
           <div class="sticky-image*">
-            <img src="/assets/img/tmp20.jpg" class="inview mt-8 lazyload-img lazyload hidden">
-            <video src="/assets/video/video2.mp4" autoplay loop muted class="w-full h-auto">
+            <video src="/assets/video/video2.mp4" autoplay loop muted class="w-full h-auto  hidden lg:block">
           </div>
         </div>
       </div>
@@ -35,7 +33,7 @@
         <div class="overflow-hidden*">
           <div class="sticky-image*">
             <div style="transform: scale(1.5); transform-origin: bottom left;">
-              <img src="/assets/img/tmp2.jpg" class="inview lazyload-img lazyload">
+              <img src="/assets/img/tmp2.jpg" class="inview lazyload-img lazyload  hidden lg:block">
             </div>
           </div>
         </div>
@@ -44,16 +42,16 @@
   </div>
   <div class="container bg-segl relative">
     <div class="u-flexgrid-g " style="min-height: 90vh">
-      <div class="col-12 lg:col-off-1 lg:col-6 lg:col-off-2 lg:col-5 text-left pt-24">
-        <h1 class="heading-xl mb-8 js-load-stagger pt-20">
-          Det personlige  konferansehuset du har ventet på
-        </h1>
-
-
-            <div class="js-load-stagger mt-8">
-              <a href="/moterom.php" class="button mb-4">Book møterom</a>
-              <a href="/konferanse.php" class="button">Book konferanse</a>
-            </div>
+      <div class="col-12 lg:col-off-1 lg:col-6 lg:col-off-2 lg:col-5 text-left lg:pt-24 front-first-txt">
+        <div class="front-first-txt__inner px-10">
+          <h1 class="heading-xl mb-8 js-load-stagger lg:pt-20">
+            Det personlige  konferansehuset du har ventet på
+          </h1>
+          <div class="js-load-stagger mt-8">
+            <a href="/moterom.php" class="button mb-4">Book møterom</a>
+            <a href="/konferanse.php" class="button">Book konferanse</a>
+          </div>
+        </div>
       </div>
     </div>
   </div>
