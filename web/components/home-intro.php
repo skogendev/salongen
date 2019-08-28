65252a1541e9344e0a30676d607496890a009bcc
Magnus Holder Bjørk <magnus@skogen.io>
2019-08-28 14:19:25 +0200

    Woops

--- web/components/home-intro.php
+++ web/components/home-intro.php
@@ -10,7 +10,7 @@
       </div>
     </div>
     <div class="w-full lg:flex-1 mob-order-1 mob-max-h-50vh intro-img relative">
-      <video class="" muted autoplay loop playsinline controls>
+      <video class="" muted autoplay loop playsinline controls poster="/assets/tmp24.jpg">
         <source src="/assets/video/introvideo.webm" type="video/webm">
         <source src="/assets/video/introvideo.mp4" type="video/mp4">
         <img src="/assets/tmp24.jpg">
