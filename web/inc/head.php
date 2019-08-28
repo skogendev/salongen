18a206388c936a2a80494238cdff23dcc15e0711
Magnus Holder Bjørk <magnus@skogen.io>
2019-08-28 13:36:10 +0200

    IE11 test

--- web/inc/head.php
+++ web/inc/head.php
@@ -1,6 +1,38 @@
 <head>
   <title>SALONGEN</title>
   <meta name="viewport" content="width=device-width,initial-scale=1">
+  <script>
+  (function detectIE() {
+    var ua = window.navigator.userAgent;
+
+    var msie = ua.indexOf('MSIE ');
+    if (msie > 0) {
+        // IE 10 or older => return version number
+        var ieV = parseInt(ua.substring(msie + 5, ua.indexOf('.', msie)), 10);
+        document.querySelector('body').className += ' ie-old';
+    }
+
+    var trident = ua.indexOf('Trident/');
+    if (trident > 0) {
+        // IE 11 => return version number
+        var rv = ua.indexOf('rv:');
+        var ieV = parseInt(ua.substring(rv + 3, ua.indexOf('.', rv)), 10);
+        document.querySelector('body').className += ' ie11';
+    }
+
+    var edge = ua.indexOf('Edge/');
+    if (edge > 0) {
+      // IE 12 (aka Edge) => return version number
+      var ieV = parseInt(ua.substring(edge + 5, ua.indexOf('.', edge)), 10);
+        document.querySelector('body').className += ' edge';
+    }
+
+    // other browser
+    return false;
+  })();
+
+  detectIE();
+  </script>
   <style type="text/css">
     @font-face {
       font-family: 'bluu';
