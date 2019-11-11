118fec449ab44e49a28a3858caf465726b4571d8
Magnus Holder Bjørk <magnus@skogen.io>
2019-11-11 13:30:21 +0100

    Share

--- web/components/home-intro.php
+++ web/components/home-intro.php
@@ -4,16 +4,14 @@
       <div class="py-8 p-2 lg:px-24 text-center lg:center-y relative mx-auto">
         <h1 class="heading-xl mb-8 js-load-stagger lg:pt-20">Velkommen til Salongen<br>&mdash; det personlige konferansehuset</h1>
         <div class="js-load-stagger mt-8 text-center">
-          <a href="/moterom.php" class="button mb-4">Book møterom</a>
+          <a href="/moterom.php" class="button mb-4"><span>Book møterom</span></a>
           <a href="/konferanse.php" class="button">Book konferanse</a>
         </div>
       </div>
     </div>
     <div class="w-full lg:flex-1 mob-order-1 mob-max-h-50vh intro-img">
-      <video class="inview lazyload-img lazyload" muted autoplay loop playsinline poster="/assets/tmp24.jpg">
-        <source src="/assets/video/introvideo.mp4" type="video/webm">
-        <source src="/assets/video/introvideo.mp4" type="video/mp4">
-        <img src="/assets/tmp24.jpg">
+      <video src="/assets/video/introvideo.mp4" class="inview lazyload-img lazyload" muted autoplay loop playsinline preload="none">
+       
       </video>
       <!--<img data-src="/assets/img/tmp24.jpg" class="inview lazyload-img lazyload">-->
     </div>  
